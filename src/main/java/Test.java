a0a12099aaf9f55a41493caf52b7bf58fa88ecc2
Jackson <Jackson@atguigu.com>
2021-10-27 20:56:25 +0800

    fixed and tested

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,15 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aaaa");
 
-        System.out.println(2131212312);
-
-        System.out.println("hot fix");
-        System.out.println(111222);
-        System.out.println(33);
-        System.out.println(444);
-
-        System.out.println("aaa");
-
+        System.out.println("nothing here");
     }
 }

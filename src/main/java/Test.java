0348e87c1628909d70eafd4ee030ae14ff04fe4c
Jackson <Jackson@atguigu.com>
2021-10-27 20:44:03 +0800

    hot fix done

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aaaa");
-
-        System.out.println(2131212312);
-
-        System.out.println("hot fix");
-        System.out.println(444);
+        System.out.println("hot fix: delete everything!");
 
 
     }
